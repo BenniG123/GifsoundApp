501d805da46cd2ab435af224fac3f1f6f2315efa
Ben Williams <bww1@iastate.edu>
2015-11-17 19:51:32 -0600

    Added in 2/3 of the changes.  Update launchpage to launch proper activity

--- app/src/main/java/cpre388/gifsound/GifSoundViewActivity.java
+++ app/src/main/java/cpre388/gifsound/GifSoundViewActivity.java
@@ -1,15 +1,19 @@
 package cpre388.gifsound;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerFragment;
 
-public class GifSoundViewActivity extends Activity {
+public class GifSoundViewActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
-    public static final String API_KEY = "AIzaSyCe6tORd9Ch4lx-9Ku5SQ476uS9OtZYsWA";
-    public static final String VIDEO_ID = "o7VVHhK9zf0";
+    public static final String API_KEY = "AIzaSyBb7IPHhczdj-FAiUN8Yli3_TQj-TDTyZI";
+    public static final String VIDEO_ID = "1HmQNkcAhgg";
 
     private YouTubePlayer youTubePlayer;
     private YouTubePlayerFragment youTubePlayerFragment;
@@ -18,6 +22,27 @@ public class GifSoundViewActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gif_sound_view);
+
+        youTubePlayerFragment = (YouTubePlayerFragment) getFragmentManager()
+                .findFragmentById(R.id.youtube_fragment);
+        youTubePlayerFragment.initialize(API_KEY, this);
     }
 
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
+        this.youTubePlayer = youTubePlayer;
+
+        if (!wasRestored) {
+            this.youTubePlayer.loadVideo(VIDEO_ID, 2000);
+        }
+
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.hide(youTubePlayerFragment);
+        ft.commit();
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
 }
