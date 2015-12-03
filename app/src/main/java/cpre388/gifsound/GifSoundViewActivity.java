4f500e36404c43c3ceffb84b83deb87bd5e3248d
Sarah Ulmer <snulmer@iastate.edu>
2015-12-03 17:02:43 -0600

    Includes html code and plays gifv's

--- app/src/main/java/cpre388/gifsound/GifSoundViewActivity.java
+++ app/src/main/java/cpre388/gifsound/GifSoundViewActivity.java
@@ -3,8 +3,10 @@ package cpre388.gifsound;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.Toast;
 
@@ -20,6 +22,21 @@ public class GifSoundViewActivity extends YouTubeBaseActivity implements YouTube
     String GifSoundLink;
     String VIDEO_ID, GIF_URL;
     int VIDEO_TIME = 0;
+
+    boolean isGifv = false;
+
+    final String HTML = "<video id=\"gif\" autoplay loop muted=\"\" poster=\"REPLACE_POSTER\" style=\"cursor: zoom-in;\">\n" +
+            "\t\t<source src=\"REPLACE_WEBM\" type=\"video/webm\">\n" +
+            "\t\t<source src=\"REPLACE_MP4\" type=\"video/mp4\">\n" +
+            "\t\tYour browser does not support HTML5 video.\n" +
+            "\t</video>";
+
+//    final String HTML = "<video autoplay=\"\" loop=\"\" muted=\"\" poster=\"REPLACE_POSTER\" >\n" +
+//            "\t\t<source src=\"REPLACE_WEBM\" type=\"video/webm\">\n" +
+//            "\t\t<source src=\"REPLACE_MP4\" type=\"video/mp4\">\n" +
+//            "\t\tYour browser does not support HTML5 video.\n" +
+//            "\t</video>";
+
 
     private YouTubePlayer youTubePlayer;
     private YouTubePlayerFragment youTubePlayerFragment;
@@ -42,9 +59,12 @@ public class GifSoundViewActivity extends YouTubeBaseActivity implements YouTube
         vg.addView(new MovieView(this));
 
         WebView image = (WebView) findViewById(R.id.web_image);
-        image.setBackgroundColor(0);
-        image.loadUrl(GIF_URL);
+//        image.setBackgroundColor(0);
+        image.setWebChromeClient(new WebChromeClient());
 
+        String html_code = writeHTMLCode();
+//        image.loadUrl(GIF_URL);
+        image.loadDataWithBaseURL("", html_code, "text/html", "UTF-8", "");
     }
 
     @Override
@@ -66,7 +86,6 @@ public class GifSoundViewActivity extends YouTubeBaseActivity implements YouTube
     }
 
     private void parseURL(String link) {
-        Toast.makeText(this, link, Toast.LENGTH_LONG).show();
         VIDEO_ID = link.substring(link.indexOf(";v=") + 3);
         if (VIDEO_ID.contains("&")) {
             VIDEO_TIME = Integer.parseInt(VIDEO_ID.substring(VIDEO_ID.indexOf(";s=")+3));
@@ -77,12 +96,24 @@ public class GifSoundViewActivity extends YouTubeBaseActivity implements YouTube
             //gifv gifs are encoded with just an id and are from i.imgur.com/ID.gif
             GIF_URL = link.substring(link.indexOf("gifv=")+5, link.indexOf("&"));
             GIF_URL = "http://i.imgur.com/"+ GIF_URL +".gifv";    //GIF_URL = website
+            isGifv = true;
         }
         else if (link.contains("gif=")) {
             //All other images are sent with gif=website
             GIF_URL = link.substring(link.indexOf("gif=")+4, link.indexOf("&"));
         }
-        Toast.makeText(this, GIF_URL, Toast.LENGTH_LONG).show();
 
+    }
+
+    private String writeHTMLCode() {
+        String code = HTML;
+//        if (isGifv) {
+            String UrlNoExtensions = GIF_URL.substring(0, GIF_URL.lastIndexOf("."));
+            code = code.replace("REPLACE_POSTER", UrlNoExtensions + ".jpg");
+            code = code.replace("REPLACE_WEBM", UrlNoExtensions + ".webm");
+            code = code.replace("REPLACE_MP4", UrlNoExtensions + ".mp4");
+            Log.d("HTML", code);
+//        }
+        return code;
     }
 }
